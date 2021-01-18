c91c5978e2a10993654533693fd35701dd7ee92f
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-18 09:21:56 -0300

    Line

--- commands/buy-bot.js
+++ commands/buy-bot.js
@@ -413,13 +413,8 @@ module.exports = {
         ) // "This command can be only called inside of a ticket"
         .then((message) => {
           setTimeout(() => {
-            let ticket = message.channel;
-            message.delete();
-            return ticket;
-          }, 5000);
-        }) //Erase when everithing works again
-        .then((ticket) => {
-          setTimeout(() => ticket.delete(), 30000);
+            message.channel.delete();
+          }, 20000);
         });
     }
 
