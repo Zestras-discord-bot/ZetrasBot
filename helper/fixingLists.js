4ea32bb6bfd7c188590940265cd0d466d93e22e5
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-04-18 13:35:52 -0300

    methods to reorder the list

---
+++ helper/fixingLists.js
@@ -0,0 +1,44 @@
+const addingIdtoObj = (categoriesId, objToModify) => {
+  return Object.keys(categoriesId)
+    .map((eachKey) => {
+      let newObj;
+      objToModify.map((eachAccountType) => {
+        if (
+          eachAccountType.name.trim() === categoriesId[eachKey].title.trim()
+        ) {
+          newObj = { ...eachAccountType, id: eachKey };
+        }
+      });
+      return newObj;
+    })
+    .filter((each) => {
+      if (each !== undefined) return each;
+    });
+};
+
+const erasingHiddenProducts = (products) => {
+  const noHiddenId = Object.keys(products).filter((eachKey) => {
+    if (!products[eachKey].hidden) {
+      return products[eachKey];
+    }
+  });
+  console.log(noHiddenId.length);
+
+  return ordenedListOfproducts(products, noHiddenId);
+};
+
+const ordenedListOfproducts = (product, noHiddenId) => {
+  return noHiddenId.map((eachId) => {
+    const newStructure = {
+      id: product[eachId]._id,
+      name: product[eachId].name,
+      minQuantity: product[eachId].minQuantity,
+      value: product[eachId].value,
+      category: product[eachId].category,
+    };
+    return newStructure;
+  });
+};
+
+module.exports.addingIdtoObj = addingIdtoObj;
+module.exports.erasingHiddenProducts = erasingHiddenProducts;
