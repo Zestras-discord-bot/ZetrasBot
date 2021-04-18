29aebd21051543361feeb57b0d77601a820892f5
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-04-18 13:34:39 -0300

    Controllers para las conneciones a la API

---
+++ helper/controller.js
@@ -0,0 +1,56 @@
+// SHOP ID
+
+const shopId = "CYTs2NH5CyGseHqsJ";
+
+// Get Categories ID  PROMISE
+
+const getCategoriesId = (DDPClient) => {
+  return new Promise((resolve, reject) =>
+    DDPClient.connect((err, reconnected) => {
+      if (err) {
+        throw err;
+      }
+
+      DDPClient.subscribe("shop.categories", [shopId], () => {
+        const categories = DDPClient.collections["shop.categories"];
+        console.log(categories);
+        return resolve(categories);
+      });
+    })
+  );
+};
+
+// Get ALL Products  PROMISE
+const getProducts = (DDPClient) => {
+  return new Promise((resolve, reject) => {
+    DDPClient.connect((err, reconnected) => {
+      if (err) {
+        throw err;
+      }
+
+      Login.loginWithToken(DDPClient, loginToken, (err, user) => {
+        const query = {
+          search: "z755JjyysMSeX3zoA",
+        };
+        const options = {
+          limit: 250, // Maximum number of products to fetch at one time. (optional)
+        };
+        DDPClient.subscribe(
+          "admin.shop.products",
+          [shopId, null, options],
+          () => {
+            const products = DDPClient.collections["shop.products"];
+            console.log(
+              Object.keys(products).length + " Products Fecth from API"
+            );
+            // console.log(products);
+            resolve(products);
+          }
+        );
+      });
+    });
+  });
+};
+
+module.exports.getCategoriesId = getCategoriesId;
+module.exports.getProducts = getProducts;
