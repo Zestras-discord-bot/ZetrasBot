719b5654acc580dda80ac6c168f9b25800dde530
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-04-20 09:56:17 -0300

    se arreglo el comando help

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,31 @@
 module.exports = {
-    async "run" (discord, bot, message, args) {
-        message.delete().catch(error => {console.log('Error start here ');console.log(error)});
-      
-        let commands = bot.baseCommands.map(command => `**${command.name}** : *${command.description}*${command.aliases ? `\nAliases: **${command.aliases.join(", ")}**` : ""}\n`);
+  run: async (discord, bot, message, args) => {
+    message.delete().catch((error) => {
+      console.log("Error start here ");
+      console.log(error);
+    });
 
-        message.channel.send(new discord.MessageEmbed()
-            .setTitle("List of Commands:")
-            .setDescription(commands)
-            .setColor(0x36393E)).catch(error => {
-                commands = commands.join("\n");
-                while(commands.includes("*")) commands = commands.replace("*", "");
-                console.log(`Commands:\n\n${commands}`);
-        });
-    },
-    "name": "help",
-    "aliases": ["halp", "cmds", "commands"],
-    "description": "Find out how to use the bot."
+    let commands = bot.baseCommands.map(
+      (command) =>
+        `**${command.name}** : *${command.description}*${
+          command.aliases ? `\nAliases: **${command.aliases.join(", ")}**` : ""
+        }\n`
+    );
+
+    message.channel
+      .send(
+        new discord.MessageEmbed()
+          .setTitle("List of Commands:")
+          .setDescription(commands)
+          .setColor(0x36393e)
+      )
+      .catch((error) => {
+        commands = commands.join("\n");
+        while (commands.includes("*")) commands = commands.replace("*", "");
+        console.log(`Commands:\n\n${commands}`);
+      });
+  },
+  name: "help",
+  aliases: ["halp", "cmds", "commands"],
+  description: "Find out how to use the bot.",
 };
