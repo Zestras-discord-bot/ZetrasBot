79a2d2fa56e290498178d903396f92cf83846e5e
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-04-20 11:02:13 -0300

    accounts y services vienen de config.json

--- helper/connectionWithDDP.js
+++ helper/connectionWithDDP.js
@@ -1,6 +1,6 @@
 const DDP = require("ddp");
 const Login = require("ddp-login");
-const loginToken = "ODKkYLF7oducs5INMuCzEBw085sKT1t20Jvb_oyAy2t";
+const { loginToken } = require("../config.json");
 
 const { addingIdtoObj, erasingHiddenProducts } = require("./fixingLists");
 const { getCategoriesId, getProducts } = require("./controller");
@@ -11,32 +11,14 @@ const DDPClient = new DDP({
   url: "wss://atshop.io/websocket",
 });
 
-let accounts = [
-  { name: "Limited Items", list: [], fileName: "LimitedItems" },
-  {
-    name: "Modern Warfare / Warzone Accounts",
-    list: [],
-    fileName: "ModernWarfare",
-  },
-  { name: "WarZone Accounts", list: [], fileName: "WarZoneAccounts" },
-  { name: "Cold War Accounts", list: [], fileName: "ColdWarAccounts" },
-  { name: "Blizzard", list: [], fileName: "Blizzard" },
-  { name: "VPN", list: [], fileName: "VPN" },
-  { name: "Porn", list: [], fileName: "Porn" },
-];
-
-let services = [
-  { name: "Instagram Services", list: [], fileName: "Instagram" },
-  { name: "TikTok", list: [], fileName: "TikTok" },
-  { name: "Twitch", list: [], fileName: "Twitch" },
-  { name: "YouTube Services", list: [], fileName: "YouTube" },
-  { name: "Call of Duty Services", list: [], fileName: "CallofDutyServices" },
-  { name: "Spotify", list: [], fileName: "Spotify" },
-];
-
 const fetchProcess = () => {
   getCategoriesId(DDPClient) // Gets The categories from API
     .then((categoriesId) => {
+      const { freshAccounts, freshServices } = require("../config.json");
+
+      let accounts = [...freshAccounts];
+      let services = [...freshServices];
+
       accounts = addingIdtoObj(categoriesId, accounts); //It matched the ID of each category with the ones we hardcoded
       services = addingIdtoObj(categoriesId, services); //It matched the ID of each category with the ones we hardcoded
 
