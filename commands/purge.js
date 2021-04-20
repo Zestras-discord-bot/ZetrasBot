295805be0d9e14f299527f63917785e69d268f88
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-04-20 11:05:47 -0300

    Se areglo el comando purge

--- commands/purge.js
+++ commands/purge.js
@@ -1,14 +1,28 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  "run": async (discord, bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**Unauthorized.**");
-    if(!args[0]) return message.channel.send("**Please specify a number of messages you want to delete !**");
-    message.channel.bulkDelete(parseInt(args[0]) !== NaN ? parseInt(args[0]) > 100 ? 100 : parseInt(args[0]) : 100).then(() => {
-      message.channel.send("Done.")
-    });
+  run: async (discord, bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+      return message.reply("**Unauthorized.**");
+    if (!args[0])
+      return message.channel.send(
+        "**Please specify a number of messages you want to delete !**"
+      );
+    message.channel
+      .bulkDelete(
+        parseInt(args[0]) !== NaN
+          ? parseInt(args[0]) > 100
+            ? 100
+            : parseInt(args[0])
+          : 100
+      )
+      .then(() => {
+        message.channel
+          .send("Done.")
+          .then((m) => setTimeout(() => m.delete(), 5000));
+      });
   },
-  "name": "purge",
-  "aliases": ["clear", "prune"],
-  "description": "Delete some messages"
-}
+  name: "purge",
+  aliases: ["clear", "prune"],
+  description: "Delete some messages",
+};
